c8a5e94efa007584b248041639171cb697b303ef
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-04-19 19:51:53 +0200

    Add the functionality of the whole app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CompaniesList from './components/companiesList';
-import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
 import NavBar from './components/NavBar';
-import Search from './components/Search';
 import './App.css';
 import CompanyProfile from './components/CompanyProfile';
 
 function App() {
-  const companyProfile = useSelector((state) => state.company.companyProfiles);
-
   return (
     <div className="main-container">
       <NavBar />
-      <Search />
       <Routes>
-        {companyProfile.length === 0 && (
-          <Route path="/" element={<CompaniesList />} />
-        )}
+        <Route path="/" element={<HomePage />} />
         <Route path="/company/:name" element={<CompanyProfile />} />
       </Routes>
     </div>
