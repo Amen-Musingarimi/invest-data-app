d3da56a302a58384c2b3db42c14198fc5f13b94a
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-04-19 09:53:43 +0200

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import CompaniesList from './components/companiesList';
+import { useSelector } from 'react-redux';
 import NavBar from './components/NavBar';
 import Search from './components/Search';
 import './App.css';
 import CompanyProfile from './components/CompanyProfile';
 
 function App() {
+  const companyProfile = useSelector((state) => state.company.companyProfiles);
+
   return (
     <div className="main-container">
-      <Provider store={store}>
-        <NavBar />
-        <Search />
-        <Routes>
+      <NavBar />
+      <Search />
+      <Routes>
+        {companyProfile.length === 0 && (
           <Route path="/" element={<CompaniesList />} />
-          <Route path="/company/:name" element={<CompanyProfile />} />
-        </Routes>
-      </Provider>
+        )}
+        <Route path="/company/:name" element={<CompanyProfile />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,52 @@
-import StockImage from './assets/background.jpg';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCompanyProfileAsync } from '../redux/companies/companiesSlice';
 import './styles/Search.css';
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const dispatch = useDispatch();
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    dispatch(getCompanyProfileAsync(searchTerm));
+  };
+
+  const companyProfile = useSelector((state) => state.company.companyProfiles);
+  console.log(companyProfile);
+
   return (
     <div className="search-container">
-      <img src={StockImage} alt="Stock" className="stock-image"></img>
       <form className="search-form">
         <input
           type="text"
+          value={searchTerm}
+          onChange={handleInputChange}
           placeholder="Search By Name e.g Chevron"
           className="search-input"
         ></input>
-        <button type="button" className="search-button">
+        <button type="button" className="search-button" onClick={handleSearch}>
           Search
         </button>
       </form>
+      <div className="search-results-list">
+        {companyProfile.map((company) => (
+          <Link
+            to={`/company/${company.name}`}
+            className="search-result-company-link"
+            onClick={() => {
+              dispatch(getCompanyProfileAsync(company.name));
+            }}
+          >
+            <span className="company-symbol">${company.profit}</span>
+            <span className="company-name">{company.name}</span>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
